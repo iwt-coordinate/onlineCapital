647c101bb9968fe46aff0a43f5b6d4f2fad5f849
haoboyu <haoboyu40@qq.com>
2018-04-03 17:13:32 +0800

    添加头部

--- src/components/head.vue
+++ src/components/head.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
-      <p>这是头部</p>
+    <header class="header">
+      <ul class="logocon clear">
+        <li class="top_logo">
+          <img src="@/images/top-logo.jpg" alt="logo" />
+        </li>
+        <li class="top_button clear">
+          <p class="topuser">abcdefghij1234567890</p>
+          <p>
+            <a class="toppass" href="#">修改密码</a>
+            <a class="topexit" href="#">安全退出</a>
+          </p>
+        </li>
+      </ul>
+    </header>
   </div>
 </template>
